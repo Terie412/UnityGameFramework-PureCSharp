b684aba443476f75db0bbe917263ac31271fd36f
qintianchen <terie.qin412@gmail.com>
2022-09-25 01:29:34 +0800

    update: content

--- client/Assets/Scripts/GamePlay/GameMain.cs
+++ client/Assets/Scripts/GamePlay/GameMain.cs
@@ -3,86 +3,71 @@ using Core;
 using Framework;
 using UnityEngine;
 
-namespace GamePlay
+public class GameMain : SingletonBehaviour<GameMain>
 {
-    public class GameMain : SingletonBehaviour<GameMain>
+    [SerializeField] private int m_FrameRate = 30;
+
+    public int frameRate
     {
-        public bool isServerEnable;
-        [SerializeField] private int m_FrameRate = 30;
-        public int frameRate
+        set
         {
-            set {
-                m_FrameRate = value;
-                Application.targetFrameRate = m_FrameRate;
-            }
+            m_FrameRate                 = value;
+            Application.targetFrameRate = m_FrameRate;
         }
+    }
 
-        private void Awake()
-        {
-            // 初始化所有的单例模式
-            GameLogger.InitSingleTonOnLoad();
-            AssetManager.InitSingleTonOnLoad();
-            ScreenAdapterManager.InitSingleTonOnLoad();
-            UIManager.InitSingleTonOnLoad();
-            NetClient.InitSingleTonOnLoad();
-            
-            GC.Collect();
-        }
+    private void Awake()
+    {
+        // 初始化所有的单例模式
+        GameLogger.InitSingleTonOnLoad();
+        AssetManager.InitSingleTonOnLoad();
+        ScreenAdapterManager.InitSingleTonOnLoad();
+        UIManager.InitSingleTonOnLoad();
+        NetClient.InitSingleTonOnLoad();
 
-        private async void Start()
-        {
-            frameRate = 30;
-            
-            GameLogger.Instance.Init();                                 // 日志系统初始化
-            await AssetManager.Instance.Init();                         // 资产管理器初始化
-            ScreenAdapterManager.Instance.Init();
-            await UIManager.Instance.InitAsync();                       // UI 管理器
-            
-            if (isServerEnable)
-            {
-                var ret = await NetClient.Instance.TryConnectToServer();    // 连接到服务器
-                if (!ret)
-                {
-                    GameLogger.LogError("连接服务器失败");
-                    return;
-                }
-            }
-    
-            Main(); // 主逻辑
-        }
-    
-        private void Update()
-        {
-            if(AssetManager.Instance.isInit)
-                AssetManager.Instance.Update();
-    
-            if (ScreenAdapterManager.Instance.isInit)
-                ScreenAdapterManager.Instance.Update();
-            
-            if(UIManager.Instance.isInit)
-                UIManager.Instance.Update();
-            
-            if(GameLogger.Instance.isInit)
-                GameLogger.Instance.Update();
-            
-            NetClient.Instance.Update();
-        }
-    
-        private void OnApplicationQuit()
-        {
-            if (GameLogger.Instance.isInit)
-                GameLogger.Instance.OnApplicationQuit();
-            
-            NetClient.Instance.OnApplicationQuit();
-        }
-    
-        private void Main()
-        {
-            // 加载主UI
-            UIManager.Instance.OpenWindow("LoginWindow", null);
-    
-            // 登录服务器
-            // LoginManager.Instance.StartLogin();
-        }
+        GC.Collect();
+    }
+
+    private async void Start()
+    {
+        frameRate = 30;
+
+        GameLogger.Instance.Init();         // 日志系统初始化
+        await AssetManager.Instance.Init(); // 资产管理器初始化
+        ScreenAdapterManager.Instance.Init(); // 屏幕适配
+        await UIManager.Instance.InitAsync(); // UI 管理器
+        
+        Main();                               // 主逻辑
+    }
+
+    private void Update()
+    {
+        if (AssetManager.Instance.isInit)
+            AssetManager.Instance.Update();
+
+        if (ScreenAdapterManager.Instance.isInit)
+            ScreenAdapterManager.Instance.Update();
+
+        if (UIManager.Instance.isInit)
+            UIManager.Instance.Update();
+
+        if (GameLogger.Instance.isInit)
+            GameLogger.Instance.Update();
+
+        NetClient.Instance.Update();
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (GameLogger.Instance.isInit)
+            GameLogger.Instance.OnApplicationQuit();
+
+        NetClient.Instance.OnApplicationQuit();
+    }
+
+    private void Main()
+    {
+        // 加载主UI
+        UIManager.Instance.OpenWindow("LoginWindow", null);
     }
 }
